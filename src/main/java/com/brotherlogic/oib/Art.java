b2da5f7ce6a7caa3b5618a47d755e46897652b29
Simon Tucker <brotherlogic@gmail.com>
2013-02-23 12:38:37 -0800

    First pass at basic system

--- src/main/java/com/brotherlogic/oib/Art.java
+++ src/main/java/com/brotherlogic/oib/Art.java
@@ -1,11 +1,67 @@
 package com.brotherlogic.oib;
 
-public class Art
+import java.io.Serializable;
+
+public class Art implements Serializable
 {
+   String address;
+   String artist;
    double latitude;
    double longitude;
-
-   String artist;
    String title;
-   String address;
+
+   public Art()
+   {
+
+   }
+
+   public String getAddress()
+   {
+      return address;
+   }
+
+   public String getArtist()
+   {
+      return artist;
+   }
+
+   public double getLatitude()
+   {
+      return latitude;
+   }
+
+   public double getLongitude()
+   {
+      return longitude;
+   }
+
+   public String getTitle()
+   {
+      return title;
+   }
+
+   public void setAddress(String address)
+   {
+      this.address = address;
+   }
+
+   public void setArtist(String artist)
+   {
+      this.artist = artist;
+   }
+
+   public void setLatitude(double latitude)
+   {
+      this.latitude = latitude;
+   }
+
+   public void setLongitude(double longitude)
+   {
+      this.longitude = longitude;
+   }
+
+   public void setTitle(String title)
+   {
+      this.title = title;
+   }
 }
