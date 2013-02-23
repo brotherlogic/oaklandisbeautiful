39945d60fa0aeca52cc6d7b34b7465fd943a67ec
Simon Tucker <brotherlogic@gmail.com>
2013-02-23 11:21:00 -0800

    First pass

---
+++ src/main/java/com/brotherlogic/oib/App.java
@@ -0,0 +1,13 @@
+package com.brotherlogic.oib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
