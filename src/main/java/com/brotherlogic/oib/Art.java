a33a958685fa020e767418091306f0b0e58d167c
Simon Tucker <brotherlogic@gmail.com>
2013-02-23 11:30:54 -0800

    Working on getting the web server working

---
+++ src/main/java/com/brotherlogic/oib/Art.java
@@ -0,0 +1,11 @@
+package com.brotherlogic.oib;
+
+public class Art
+{
+   double latitude;
+   double longitude;
+
+   String artist;
+   String title;
+   String address;
+}
--- src/main/java/com/brotherlogic/oib/Database.java
+++ src/main/java/com/brotherlogic/oib/Database.java
@@ -4,7 +4,7 @@ package com.brotherlogic.oib;
  * Hello world!
  *
  */
-public class App 
+public class Database 
 {
     public static void main( String[] args )
     {
